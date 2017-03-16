ff0624984155d5be2c929cd8c0243963a3094620
sunshineKatie <kcbubblegum35@gmail.com>
2017-03-16 19:27:28 -0400

    put functions from practice robot to defense robot, making changes to autonomus to turn with the gyro- currently not working

--- src/DefenseRobot.cpp
+++ src/DefenseRobot.cpp
@@ -5,6 +5,15 @@
 #define leftY 1
 #define rightY 3
 
+	void StartExcellLogging()//hopefully will be used in VS for a reason to start data logging when we get there
+	{
+	printf("\n <$EO$> Lenc, Renc, Gyro, Accel, Setpoint, ProcessData, Output, Auto_Sw, Run_Mode, Method, Time");
+	}
+	void StopExcellLoging()//will be used in VS for a reason to stop data logging
+	{
+	printf("\n <$EO$> STOPLOG");
+	}
+
 #define STOP 0.0
 #define CIM_RPM 5000
 class Robot: public SampleRobot
@@ -78,195 +87,184 @@ public:
 		printf("\n gyro:%f", gyro.GetAngle());
 
 	}
-// Miscellaneous functions
-	// math functions
-	float Map(float x, float in_min, float in_max, float out_min, float out_max){
-	        // use this function to match two value's scales proportionally
-	        return ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
-	    }
-	float Limit(float num)
-	{
-		   if (num > 1.0)
-		   {
-				   return 1.0;
-		   }
-		   if (num < -1.0)
-		   {
-				   return -1.0;
-		   }
-		   return num;
-	}
-	float SetRPM( int RPM_SetPoint, int RPM_Current, int MaxRPM)//should always be positive, accumulating inputs, run in a timed loop
-	{
-		//initialize variables
-		float MotorOutput=0;
-		float kp=0.008;
-		float BaseSpeed=RPM_SetPoint/MaxRPM-0.05;//just slower theoretically than the desired output ish...
-		float nError=RPM_SetPoint-RPM_Current;
-			if(nError<0)//HOLD BASE SPEED
-			{
-				nError=0;
-			}
-			MotorOutput=kp*nError;
-
-			if((MotorOutput<BaseSpeed))
-			{
-				MotorOutput=BaseSpeed;//prevents us from oscillating around 0
-			}
-			else if(MotorOutput>1)
-			{
-				MotorOutput=BaseSpeed+(0.25*BaseSpeed);//handles exception for motor capacity
-			}
-		return MotorOutput;
-	}
-	int GetRpm(int NFeedback, int CPR, float LoopTime)
-	{
-		return ( (abs(NFeedback)/CPR)*(LoopTime*60) );
-	}
-	float Float_abs(float in)
-	{
-
-		return ( in*( (in<0)*(-1)+(in>0) ) );// -40*((1)*-1)+0=40; 40*(((0)*-1)+1)=40   ABS WORKS!!!!
-	}
-//functions for drivetrain
-	void SetSpeed(float Rspeed, float Lspeed)					// tested --working on final
-	{
-		Right1.Set(-Rspeed);
-		Right2.Set(-Rspeed);
-		Left1.Set(Lspeed);
-		Left2.Set(Lspeed);
-	}
-	void SetSpeed(float speed)									// tested --working on final
-	{
-		SetSpeed(speed, speed);  //(-)Right, (+) Left
-	}
-	void DriveFRC(float outputMagnitude, float curve)			// tested --working on final
+	// Miscellaneous functions- that do not call Outputs
+		float Map(float x, float in_min, float in_max, float out_min, float out_max){
+		        // use this function to match two value's scales proportionally
+		        return ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
+		    }
+		float Limit(float num)
 		{
-			float leftOutput, rightOutput;
-			float m_sensitivity = 0.5;
-			if (curve < 0)
-			{
-			   float value = log(-curve);
-			   float ratio = (value - m_sensitivity)/(value + m_sensitivity);
-			   if (ratio == 0) ratio =.0000000001;
-			  leftOutput = outputMagnitude / ratio;
-			  rightOutput = outputMagnitude;
-			}
-			else if (curve > 0)
-			{
-			  float value = log(curve);
-			  float ratio = (value - m_sensitivity)/(value + m_sensitivity);
-			   if (ratio == 0) ratio =.0000000001;
-			   leftOutput = outputMagnitude;
-			   rightOutput = outputMagnitude / ratio;
-			}
-			else
-			{
-			  leftOutput = outputMagnitude;
-			  rightOutput = outputMagnitude;
-			}
-			SetSpeed(rightOutput, leftOutput);
+			   if (num > 1.0)
+			   {
+					   return 1.0;
+			   }
+			   if (num < -1.0)
+			   {
+					   return -1.0;
+			   }
+			   return num;
 		}
-	void drivestraightwithencoders(float target, float speed)	// tested --working on final
-		{ //if need to change because it is not working, use Lenc instead of right, and test it to make sure it counts forward
+		float SetRPM( int RPM_SetPoint, int RPM_Current, int MaxRPM)//should always be positive, accumulating inputs, run in a timed loop
+		{
+			//initialize variables
+			float MotorOutput=0;
+			float kp=0.008;
+			float BaseSpeed=RPM_SetPoint/MaxRPM-0.05;//just slower theoretically than the desired output ish...
+			float nError=RPM_SetPoint-RPM_Current;
+				if(nError<0)//HOLD BASE SPEED
+				{
+					nError=0;
+				}
+				MotorOutput=kp*nError;
+
+				if((MotorOutput<BaseSpeed))
+				{
+					MotorOutput=BaseSpeed;//prevents us from oscillating around 0
+				}
+				else if(MotorOutput>1)
+				{
+					MotorOutput=BaseSpeed+(0.25*BaseSpeed);//handles exception for motor capacity
+				}
+			return MotorOutput;
+		}
+		int GetRpm(int NFeedback, int CPR, float LoopTime)
+		{
+			return ( (abs(NFeedback)/CPR)*(LoopTime*60) );
+		}
+		float Float_abs(float in)
+		{
+
+			return ( in*( (in<0)*(-1)+(in>0) ) );// -40*((1)*-1)+0=40; 40*(((0)*-1)+1)=40   ABS WORKS!!!!
+		}
+		void ExcellOut(float Setpoint, float ProcessData, float Output,float etc1,float etc2, int MethodInUse)// <$EO$> is a special tag so we can tell when to output a chunk of data
+		{
+			printf("\n <$EO$> %i, %i, %f, %f, %f, %f, %f, %f, %f, %f, %f ", Lenc->Get(), Renc->Get(), gyro.GetAngle(), 0.0, Setpoint, ProcessData, Output, etc1, etc2, MethodInUse, 0.0);
+		}
+		//functions for drivetrain
+
+		void SetSpeed(float Rspeed, float Lspeed)					// tested --working on final
+		{
+			Right1.Set(-Rspeed);
+			Right2.Set(-Rspeed);
+			Left1.Set(Lspeed);
+			Left2.Set(Lspeed);
+		}
+		void SetSpeed(float speed)									// tested --working on final
+		{
+			SetSpeed(speed, speed);  //(-)Right, (+) Left
+		}
+		void DriveFRC(float outputMagnitude, float curve)			// tested --working on final
+			{
+				float leftOutput, rightOutput;
+				float m_sensitivity = 0.5;
+				if (curve < 0)
+				{
+				   float value = log(-curve);
+				   float ratio = (value - m_sensitivity)/(value + m_sensitivity);
+				   if (ratio == 0) ratio =.0000000001;
+				  leftOutput = outputMagnitude / ratio;
+				  rightOutput = outputMagnitude;
+				}
+				else if (curve > 0)
+				{
+				  float value = log(curve);
+				  float ratio = (value - m_sensitivity)/(value + m_sensitivity);
+				   if (ratio == 0) ratio =.0000000001;
+				   leftOutput = outputMagnitude;
+				   rightOutput = outputMagnitude / ratio;
+				}
+				else
+				{
+				  leftOutput = outputMagnitude;
+				  rightOutput = outputMagnitude;
+				}
+				SetSpeed(rightOutput, leftOutput);
+			}
+		void drivestraightwithencoders(float target, float speed)	// tested --working on final
+			{ //if need to change because it is not working, use Lenc instead of right, and test it to make sure it counts forward
+				gyro.Reset();
+				Renc->Reset();
+				Lenc->Reset();
+				Renc->Reset();
+				ExcellOut(target,0,0,0,0,1);
+				Wait(0.25);
+				float kp = 0.125;
+				int enc =0;
+					if(target>0)//forward incrementing positive
+					{
+						while((target>enc)&&(IsAutonomous()&&IsEnabled()))
+						{
+						float correction=kp*-1*gyro.GetAngle()+0.15;
+						enc=Renc->Get();
+						DriveFRC(speed, correction);
+
+						ExcellOut(target,correction,0,0,0,1);
+						Wait(0.001);
+						}
+					}
+					if (target<0)//reverse incrementing negative			//UNTESTED
+					{
+						while((target<enc)&&(IsAutonomous()&&IsEnabled()))
+						{
+						float correction=kp*gyro.GetAngle()+0.15;
+						enc=Renc->Get();
+						DriveFRC(-speed, correction);
+
+						ExcellOut(target,correction,0,0,0,1);
+						Wait(0.001);
+						}
+					}
+				DriveFRC(0.0,0.0);
+			}
+		void Drive(float distance)									// tested --working on final( +- 3/8 in)
+	//used to drive straight with encoders (measured in inches)
+			{
+				float wheel_radius =2.2;
+				float wheel_circumference = 2*M_PI*wheel_radius;
+				int PPR = 360;
+				float enc_in = PPR/wheel_circumference;
+				float Target = distance*enc_in;
+				drivestraightwithencoders(Target, 0.35);
+			}
+		void GyroTurnRight(int angle)
+		{
+			float speed= 0.25;
+			// for testing the gyro value
+	// 90 to the right is +90
+	//90 to the left is -90
 			gyro.Reset();
-			Renc->Reset();
-			Wait(0.25);
-			float kp = 0.125;
-			int enc =0;
-				if(target>0)//forward incrementing positive
-				{
-					while((target>enc)&&(IsAutonomous()&&IsEnabled()))
-					{
+			Wait(0.1);
+			ExcellOut(angle,0,0,0,0,2);
+			while ((gyro.GetAngle()<angle)&&(IsAutonomous()&&IsEnabled())) //turn right 45 degrees  Tested 2/18- works; to go 90 degrees set it to 84
+			{
+				ExcellOut(angle,0,0,0,0,0);
+				SetSpeed(-speed,speed);
+			}
+			SetSpeed(STOP);
 
-					enc=Renc->Get();
-					printf("\n Renc:%i",enc);
-					printf("\n -------Angle------%f", gyro.GetAngle());
-					DriveFRC(speed, kp*-1*gyro.GetAngle()+0.15);
-					Wait(0.001);
-					}
-				}
-				if (target<0)//reverse incrementing negative			//UNTESTED
-				{
-					while((target<enc)&&(IsAutonomous()&&IsEnabled()))
-					{
-					enc=Renc->Get();
-					printf("\n enc:%i",enc);
-					DriveFRC(-speed, kp*gyro.GetAngle()+0.15);
-					Wait(0.001);
-					}
-				}
-			DriveFRC(0.0,0.0);
 		}
-	void Drive(float distance)									// tested --working on final( +- 3/8 in)
-//used to drive straight with encoders (measured in inches)
+		void GyroTurnLeft(int angle)
+
 		{
-			float wheel_radius =2.2;
-			float wheel_circumference = 2*M_PI*wheel_radius;
-			int PPR = 360;
-			float enc_in = PPR/wheel_circumference;
-			float Target = distance*enc_in;
-			Lenc->Reset();
-			Renc->Reset();
-			printf("\n -------------encoders reset------------");
-			printf("\n Target:%f",Target);
-			drivestraightwithencoders(Target, 0.35);
+			float speed= 0.25;
+			ExcellOut(angle,0,0,0,0,3);
+			gyro.Reset();
+			Wait(0.1);
+			while ((gyro.GetAngle()> (-1*angle))&&(IsAutonomous()&&IsEnabled())) //turn left 45 degrees
+			{
+				ExcellOut(angle,0,0,0,0,0);
+				SetSpeed(speed,-speed);
+			}
+			SetSpeed(STOP);
 
 		}
-	void Turn (float angle)				//needs to be tested must be within 1 degree consistently
-		{//right only
-//		float speed= 0.25;
-//				// for testing the gyro value
-//		// 90 to the right is +90
-//		//90 to the left is -90
-//			gyro.Reset();
-//			Wait(0.1);
-//			float error=angle-gyro.GetAngle());
-//			while ((gyro.GetAngle()!=angle)&&(IsAutonomous()&&IsEnabled())) //turn right 45 degrees  Tested 2/18- works; to go 90 degrees set it to 84
-//			{
-//
-//			}
-//			SetSpeed(STOP);
-
-
-		}
-	void GyroTurnRight(int angle)
-	{
-		float speed= 0.25;
-		// for testing the gyro value
-// 90 to the right is +90
-//90 to the left is -90
-		gyro.Reset();
-		Wait(0.1);
-
-
-		while ((gyro.GetAngle()<angle)&&(IsAutonomous()&&IsEnabled())) //turn right 45 degrees  Tested 2/18- works; to go 90 degrees set it to 84
-		{
-			SetSpeed(-speed,speed);
-		}
-		SetSpeed(STOP);
-
-	}
-	void GyroTurnLeft(int angle)
-	{
-		float speed= 0.25;
-
-		gyro.Reset();
-		Wait(0.1);
-
-		while ((gyro.GetAngle()> (-1*angle))&&(IsAutonomous()&&IsEnabled())) //turn left 45 degrees
-		{
-			SetSpeed(speed,-speed);
-		}
-		SetSpeed(STOP);
-
-	}
-	void DriveCoast()
+		void DriveCoast()
 		{
 			Left1.ConfigNeutralMode(CANSpeedController::NeutralMode::kNeutralMode_Coast);
 			Left2.ConfigNeutralMode(CANSpeedController::NeutralMode::kNeutralMode_Coast);
 			Right1.ConfigNeutralMode(CANSpeedController::NeutralMode::kNeutralMode_Coast);
 			Right2.ConfigNeutralMode(CANSpeedController::NeutralMode::kNeutralMode_Coast);
+
 
 		}
 		void DriveBrake()
@@ -275,80 +273,80 @@ public:
 			Left2.ConfigNeutralMode(CANSpeedController::NeutralMode::kNeutralMode_Brake);
 			Right1.ConfigNeutralMode(CANSpeedController::NeutralMode::kNeutralMode_Brake);
 			Right2.ConfigNeutralMode(CANSpeedController::NeutralMode::kNeutralMode_Brake);
+
 		}
-// functions for arm
-	void Arm_Up()
-	{
-		Arm_floor.Set(DoubleSolenoid::kForward);		//arm is up
-		Arm_peg.Set(DoubleSolenoid::kForward); //both pneumatic cylinders are closed
-		Wait(0.05);
-		Gripper.Set(DoubleSolenoid::kReverse); //close gripper
-	}
-	void Arm_Mid()
-	{
-		Arm_floor.Set(DoubleSolenoid::kForward); //floor pneumatic cylinder is closed //arm is at middle position
-		Arm_peg.Set(DoubleSolenoid::kReverse); //arm peg pneumatic cylinder is open
-		Wait(0.25);
-		Gripper.Set(DoubleSolenoid::kForward);//open gripper
-		Wait(0.05);
-	}
-	void Arm_Down()
-	{
-		Arm_floor.Set(DoubleSolenoid::kReverse);  //arm is down
-		Arm_peg.Set(DoubleSolenoid::kReverse); //both pneumatic cylinders are open
-		Wait(0.05);
-	}
-//autonomous procedures below
-	void initializeRobot()  //initialization, use this before autonomous and Operator control
-	{
+	// functions for arm
+		void Arm_Up()
+		{
+			Arm_floor.Set(DoubleSolenoid::kForward);		//arm is up
+			Arm_peg.Set(DoubleSolenoid::kForward); //both pneumatic cylinders are closed
+			Wait(0.05);
+			Gripper.Set(DoubleSolenoid::kReverse); //close gripper
+		}
+		void Arm_Mid()
+		{
+			Arm_floor.Set(DoubleSolenoid::kForward); //floor pneumatic cylinder is closed //arm is at middle position
+			Arm_peg.Set(DoubleSolenoid::kReverse); //arm peg pneumatic cylinder is open
+			Wait(0.25);
+			Gripper.Set(DoubleSolenoid::kForward);//open gripper
+			Wait(0.05);
+		}
+		void Arm_Down()
+		{
+			Arm_floor.Set(DoubleSolenoid::kReverse);  //arm is down
+			Arm_peg.Set(DoubleSolenoid::kReverse); //both pneumatic cylinders are open
+			Wait(0.05);
+		}
+	//autonomous procedures below
+		void initializeRobot()  //initialization, use this before autonomous and Operator control
+		{
 
-		Arm_Up();
-		Gripper.Set(DoubleSolenoid::kReverse); //close gripper
-		Funnel.Set(DoubleSolenoid::kReverse); // close funnel
-	}
-	void Peg_Left()  //autonomus for putting the gear on the left peg
-	{
-		initializeRobot();
-		Drive(83);
-		Wait(1.50);
-		GyroTurnRight(50);
-		Drive(34);
-		Wait(1.50);
-		Arm_Mid();  //dropping off the gear
-		Drive(-34);
-		Arm_Up();
-		GyroTurnLeft(50);
-		drivestraightwithencoders((10*360),1.0);//drive like a bat out of hell for 10 feet.
-	}
-	void Peg_Center()// tested- working
-	{
-		initializeRobot();
-		Drive(80);
-		Wait(0.125);
-		Arm_Mid(); //drop off gear
-		//below this not tested
-		Drive(-50);
-		GyroTurnRight(90);
-		Drive(70);
-		GyroTurnLeft(90);
-		Drive(300);
-	}
-	void Peg_Right()
-	{
-		initializeRobot();
-		Drive(83);//-7
-		Wait(1.50);
-		GyroTurnLeft(50);
-		Drive(32);
-		Wait(1.50);
-		Arm_Mid(); //drop off gear
-		Drive(-32);
-		Arm_Up();
-		GyroTurnRight(50);
-		drivestraightwithencoders((10*360),1.0);//drive like a bat out of hell for 10 feet.
+			Arm_Up();
+			Gripper.Set(DoubleSolenoid::kReverse); //close gripper
+			Funnel.Set(DoubleSolenoid::kReverse); // close funnel
+		}
+		void Peg_Left()  //autonomus for putting the gear on the left peg
+		{
+			initializeRobot();
+			Drive(83);
+			Wait(1.50);
+			GyroTurnRight(50);
+			Drive(34);
+			Wait(1.50);
+			Arm_Mid();  //dropping off the gear
+			Drive(-34);
+			Arm_Up();
+			GyroTurnLeft(50);
+			drivestraightwithencoders((10*360),1.0);//drive like a bat out of hell for 10 feet.
+		}
+		void Peg_Center()// tested- working
+		{
+			initializeRobot();
+			Drive(80);
+			Wait(0.125);
+			Arm_Mid(); //drop off gear
+			//below this not tested
+			Drive(-50);
+			GyroTurnRight(90);
+			Drive(70);
+			GyroTurnLeft(90);
+			Drive(300);
+		}
+		void Peg_Right()
+		{
+			initializeRobot();
+			Drive(83);//-7
+			Wait(1.50);
+			GyroTurnLeft(50);
+			Drive(32);
+			Wait(1.50);
+			Arm_Mid(); //drop off gear
+			Drive(-32);
+			Arm_Up();
+			GyroTurnRight(50);
+			drivestraightwithencoders((10*360),1.0);//drive like a bat out of hell for 10 feet.
 
-	}
-//DRVERSTATION METHODS-- code entry-points below
+		}
 	void Autonomous()
 	{ //for the autonomus switch- needs to be tested witht the printf
 		int Auto_Sel=Map(Mode_Pot.GetVoltage(), 0, 5, 1, 12);  //0 is the input min, 5 is the input max (5Volts), 1 is the output max, 12 is the output max
@@ -362,34 +360,36 @@ public:
 				LightRed->Set(Relay::Value::kOff); //turn the other lights off
 				LightGreen->Set(Relay::Value::kForward); //turn green light on
 				LightBlue->Set(Relay::Value::kOff); //put the lights in the same order thoughout the code!
+				printf("Left");
 
-				Peg_Left(); //use peg left
+				GyroTurnRight(87);
+			//	Peg_Left(); //use peg left
 			}
 			else if (Auto_Sel==2)//if it is in position 2
 			{
 				LightRed->Set(Relay::Value::kOff);
 				LightGreen->Set(Relay::Value::kForward);  //turn green light on
 				LightBlue->Set(Relay::Value::kOff);
-
-				Peg_Center(); //use peg center
+				printf("Center");
+				//Peg_Center(); //use peg center
 			}
 			else if (Auto_Sel==3)
 			{
 				LightRed->Set(Relay::Value::kOff);
 				LightGreen->Set(Relay::Value::kForward); //turn the green light on
 				LightBlue->Set(Relay::Value::kOff);
-
-				Peg_Right();
+				printf("Right");
+				//Peg_Right();
 			}
 
 			else if (Auto_Sel==4)
 			{
+
 				LightRed->Set(Relay::Value::kOff);
 				LightGreen->Set(Relay::Value::kOff); //turn the green light on
 				LightBlue->Set(Relay::Value::kOff);
 
-				initializeRobot();
-				GyroTurnRight(87);
+
 
 			}
 			else  //if we did not select any of the 4
@@ -398,6 +398,8 @@ public:
 				LightGreen->Set(Relay::Value::kOff);//turnt the other lights off
 				LightBlue->Set(Relay::Value::kOff);// do not do anything other than turning the light on
 
+			//	initializeRobot();
+				//GyroTurnRight(87);
 			}
 		}
 
@@ -489,6 +491,8 @@ public:
 							timeElapsed=0;
 						}
 						Funnel.Set(DoubleSolenoid::kReverse);
+
+
 						Wait(0.05);//retract
 					}else if(Funnel_Cycle==2)
 					{
